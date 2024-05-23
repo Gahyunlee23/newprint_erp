1f6f2c08a10f2c86898f7e39b98602f967239527
gahyun <gahyunlee1119@gmail.com>
2024-05-23 18:07:21 -0400

    adding app color style

---
+++ lib/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  // Primary colors
+  static const Color primaryColor = Color(0xff1176c0);
+  static const Color secondaryColor = Color(0xff535f70);
+  static const Color errorColor = Color(0xffba1a1a);
+
+}
